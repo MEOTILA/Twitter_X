13d8d26ffa506425d5aced01e256892bbbff241e
Sattar <sattargh@aol.com>
2024-11-10 22:35:18 +0330

    Commit added Menu for SignUp and Login

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,73 @@
 package org.example;
 
+import org.example.entity.User;
+import org.example.repository.TagRepository;
+import org.example.repository.TweetRepository;
+import org.example.repository.UserRepository;
+import org.example.services.UserServices;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws SQLException {
+
+//        UserRepository userRepository = new UserRepository();
+//        TweetRepository tweetRepository = new TweetRepository();
+//        TagRepository tagRepository = new TagRepository();
+        UserServices userServices = new UserServices();
+
+
+        System.out.println("Welcome to Twitter!");
+        while (true) {
+            System.out.println("1. Signup");
+            System.out.println("2. Login");
+            System.out.println("Choose You Action: ");
+            String option = scanner.next();
+
+            switch (option) {
+                case "1":
+                    System.out.println("Please Enter Your Username:");
+                    String username = scanner.next();
+                    System.out.println("Please Enter Your Password:");
+                    String password = scanner.next();
+                    System.out.println("Please Enter Your Display Name:");
+                    String displayName = scanner.next();
+                    System.out.println("Please Enter Your Email:");
+                    String email = scanner.next();
+                    System.out.println("Please Enter Your Bio:");
+                    String bio = scanner.next();
+                    userServices.userSignUp(username, password, displayName, email, bio);
+                    System.out.println("Your Account has been created!");
+                    break;
+
+                case "2":
+                    System.out.println("Please Enter Your Username or Email: ");
+                    String usernameOrEmail = scanner.next();
+                    System.out.println("Please Enter Your Password: ");
+                    String LoggingPassword = scanner.next();
+                    userServices.userLogin(usernameOrEmail, LoggingPassword);
+                    break;
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+//        String username = "Sattar";
+//        String password = "123";
+//        String displayName = "MEOTILA";
+//        String email = "sattar@gmail.com";
+//        String bio = "Hello! I'm Sattar!";
+//
+//        userServices.userSignUp(username, password, displayName, email, bio);
+//        userServices.userLogin("Sattar",
+//                "$2a$06$ufppY88TDjte4T3REHjPGe5fzr1PosiPop8gEBpmSLnxX3N7FQgke");
+//    }
