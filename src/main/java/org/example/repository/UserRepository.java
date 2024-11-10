7f0e93e29a490d2952ddf299b11ab10da22a623b
Sattar <sattargh@aol.com>
2024-11-10 22:34:43 +0330

    Commit added User Entity and it's Repository

---
+++ src/main/java/org/example/repository/UserRepository.java
@@ -0,0 +1,165 @@
+package org.example.repository;
+
+import org.example.Datasource;
+import org.example.entity.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class UserRepository {
+
+    private static final String CREATE_TABLE_USER = """
+            CREATE TABLE IF NOT EXISTS users (
+            user_id SERIAL PRIMARY KEY,
+            username varchar(250) UNIQUE NOT NULL,
+            password varchar(250) NOT NULL,
+            display_name varchar(250) NOT NULL,
+            email varchar(50) UNIQUE NOT NULL,
+            bio varchar(250),
+            account_create_Date Date NOT NULL);
+            """;
+
+    public UserRepository() throws SQLException {
+        // Initialize table on creation
+        initTable();
+    }
+
+    public void initTable() throws SQLException {
+        var statement = Datasource.getConnection().prepareStatement(UserRepository.CREATE_TABLE_USER);
+        statement.execute();
+        statement.close();
+    }
+    private static final String INSERT_SQL = """
+            INSERT INTO users(username, password, display_name, email, bio, account_create_date)
+            VALUES (?, crypt(?, gen_salt('bf')), ?, ?, ?, ?)
+            """;
+    private static final String DELETE_BY_ID = """
+            DELETE FROM USERS
+            WHERE user_id = ?
+            """;
+
+    private static final String FIND_BY_ID = """
+            SELECT * FROM USERS
+            WHERE user_id = ?
+            """;
+
+    private static final String FIND_BY_USERNAME = """
+            SELECT * FROM USERS
+            WHERE username = ?
+            """;
+    private static final String FIND_BY_EMAIL = """
+            SELECT * FROM USERS
+            WHERE email = ?
+            """;
+
+    private static final String UPDATE_USER = """
+            UPDATE USERS
+            SET username = ? , password = crypt(?, gen_salt('bf')) , display_name = ? , bio = ?
+            WHERE user_id = ?;
+            """;
+
+    public User save(User user) throws SQLException {
+        var statement = Datasource.getConnection().prepareStatement(INSERT_SQL);
+        statement.setString(1, user.getUsername());
+        statement.setString(2, user.getPassword());
+        statement.setString(3, user.getDisplayName());
+        statement.setString(4, user.getEmail());
+        statement.setString(5, user.getBio());
+        java.sql.Date sqlDate = java.sql.Date.valueOf(user.getAccountCreateDate());
+        statement.setDate(6, sqlDate);
+
+        statement.execute();
+        statement.close();
+        return user;
+    }
+
+    public void deleteById(int id) throws SQLException {
+        try (var statement = Datasource.getConnection().prepareStatement(DELETE_BY_ID)) {
+            statement.setLong(1, id);
+            var affectedRows = statement.executeUpdate();
+            System.out.println("# of user deleted: " + affectedRows);
+        }
+    }
+
+    public User findById(int id) throws SQLException {
+        try (var statement = Datasource.getConnection().prepareStatement(FIND_BY_ID)) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            User user = null;
+            if (resultSet.next()) {
+                int user_id = resultSet.getInt(1);
+                String username = resultSet.getString(2);
+                String password = resultSet.getString(3);
+                String display_name = resultSet.getString(4);
+                String email = resultSet.getString(5);
+                String bio = resultSet.getString(6);
+                LocalDate account_create_date = resultSet.getDate(7).toLocalDate();
+
+                user = new User (user_id, username, password, display_name,
+                        email, bio, account_create_date);
+            }
+
+            return user;
+        }
+    }
+
+    public User findByUsername(String username) throws SQLException {
+        try (var statement = Datasource.getConnection().prepareStatement(FIND_BY_USERNAME)) {
+            statement.setString(1, username);
+            ResultSet resultSet = statement.executeQuery();
+
+            User user = null;
+            if (resultSet.next()) {
+                int user_id = resultSet.getInt(1);
+                String userName = resultSet.getString(2);
+                String password = resultSet.getString(3);
+                String display_name = resultSet.getString(4);
+                String email = resultSet.getString(5);
+                String bio = resultSet.getString(6);
+                LocalDate account_create_date = resultSet.getDate(7).toLocalDate();
+
+                user = new User (user_id, userName, password, display_name,
+                        email, bio, account_create_date);
+            }
+
+            return user;
+        }
+    }
+    public User findByEmail(String email) throws SQLException {
+        try (var statement = Datasource.getConnection().prepareStatement(FIND_BY_EMAIL)) {
+            statement.setString(1, email);
+            ResultSet resultSet = statement.executeQuery();
+
+            User user = null;
+            if (resultSet.next()) {
+                int user_id = resultSet.getInt(1);
+                String userName = resultSet.getString(2);
+                String password = resultSet.getString(3);
+                String display_name = resultSet.getString(4);
+                String eMail = resultSet.getString(5);
+                String bio = resultSet.getString(6);
+                LocalDate account_create_date = resultSet.getDate(7).toLocalDate();
+
+                user = new User (user_id, userName, password, display_name,
+                        eMail, bio, account_create_date);
+            }
+
+            return user;
+        }
+    }
+
+    public User updateUser(User user) throws SQLException {
+        var statement = Datasource.getConnection().prepareStatement(UPDATE_USER);
+        statement.setString(1, user.getUsername());
+        statement.setString(2, user.getPassword());
+        statement.setString(3,user.getDisplayName());
+        statement.setString(4,user.getBio());
+
+        statement.execute();
+        statement.close();
+        return user;
+    }
+}
---
+++ src/main/java/org/example/services/UserServices.java
@@ -0,0 +1,48 @@
+package org.example.services;
+
+import org.example.entity.User;
+import org.example.repository.UserRepository;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class UserServices {
+    //Login
+    //SignUp
+    UserRepository userRepository;
+    public UserServices() throws SQLException {
+        userRepository = new UserRepository();
+    }
+    public User userSignUp(String username, String password, String displayName,
+                           String email, String bio) throws SQLException {
+        var checkingUser = userRepository.findByUsername(username);
+
+        if (checkingUser != null) {
+            System.out.println("Username is already taken!");
+            return null;
+        }
+        User signingUpUser = new User();
+        signingUpUser.setUsername(username);
+        signingUpUser.setPassword(password);
+        signingUpUser.setDisplayName(displayName);
+        signingUpUser.setEmail(email);
+        signingUpUser.setBio(bio);
+        signingUpUser.setAccountCreateDate(LocalDate.now());
+        return userRepository.save(signingUpUser);
+    }
+
+    public boolean userLogin(String usernameOrEmail, String password) throws SQLException {
+        var checkingUserByUsername = userRepository.findByUsername(usernameOrEmail);
+        //var checkingUserByEmail = userRepository.findByEmail(usernameOrEmail);
+
+        if (checkingUserByUsername != null) {
+            if (checkingUserByUsername.getPassword().equals(password)) {
+                AuthenticationServices.setLoggedUser(checkingUserByUsername);
+                System.out.println("Welcome to Tweeter!");
+                return true;
+            }
+        }
+        System.out.println("Wrong username or password!");
+        return false;
+    }
+}
